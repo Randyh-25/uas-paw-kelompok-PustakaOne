1c413765b8b95795b078b364867100ebbc70f047
Syuhada Rantisi <suadarantisi@gmail.com>
2025-12-17 12:46:00 +0700

    Enable cors for production vercel

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,8 @@ from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 from sqlalchemy.orm import sessionmaker
 from zope.sqlalchemy import register as zope_register
+from pyramid.events import NewResponse
+
 
 from .models import Base
 
@@ -20,6 +22,25 @@ def get_tm_session(session_factory, transaction_manager):
     zope_register(dbsession, transaction_manager=transaction_manager)
     return dbsession
 
+def cors_response_callback(event):
+    request = event.request
+    response = event.response
+
+    origin = request.headers.get("Origin")
+    allowed_origins = (
+        "https://pustakaone.vercel.app",
+        "http://localhost:5173",
+        "http://localhost:6543",
+    )
+
+    if origin in allowed_origins:
+        response.headers.update({
+            "Access-Control-Allow-Origin": origin,
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Credentials": "true",
+        })
+
 
 def main(global_config, **settings):
     """Pyramid application factory."""
@@ -28,6 +49,9 @@ def main(global_config, **settings):
     Base.metadata.bind = engine
 
     with Configurator(settings=settings) as config:
+        # Configure CORS
+        config.add_subscriber(cors_response_callback, NewResponse)
+
         config.include("pyramid_retry")
         config.include("pyramid_tm")
         config.add_request_method(
